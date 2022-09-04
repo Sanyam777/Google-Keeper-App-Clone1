70a508c95771c50c6a117214fb2c5e416ad4c7ba
Sanyam <sanyammrd56@gmail.com>
2022-09-04 19:16:49 +0530

    First commit

---
+++ build/precache-manifest.1e43e7fc530c7b03c34f45816be74e76.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ca8945500da39635bf37b5ede89974",
+    "url": "/Google-Keeper-App-Clone/index.html"
+  },
+  {
+    "revision": "f5115e9113d34e11d7de",
+    "url": "/Google-Keeper-App-Clone/static/js/2.88e6856e.chunk.js"
+  },
+  {
+    "revision": "e076337ce8e9147928e1",
+    "url": "/Google-Keeper-App-Clone/static/js/main.ff371806.chunk.js"
+  },
+  {
+    "revision": "58c38d4911c616e82643",
+    "url": "/Google-Keeper-App-Clone/static/js/runtime-main.6b10b978.js"
+  }
+]);
